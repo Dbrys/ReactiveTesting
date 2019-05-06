2a954516e668811a7834eb7a34af515e72a298c3
Dillon Brys <dbrys@biztechnologysolutions.com>
2019-05-06 12:56:35 -0400

    Debugging CombineLatest Operator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BandListComponent } from '../app/band-list/band-list.component';
+
+const routes: Routes = [ 
+  {path: 'bands', component: BandListComponent}, 
+  {path: '', redirectTo: 'bands', pathMatch: 'full'}
+]; 
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { BandListComponent } from '../app/band-list/band-list.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/band-list/band-list.component.ts
+++ src/app/band-list/band-list.component.ts
@@ -2,35 +2,43 @@ import { Component, OnInit } from '@angular/core';
 
 import { BandDataService } from '../band-data.service';
 import { Band } from '../models';
-import { Observable, interval, Subject, merge } from 'rxjs';
+import { Observable, interval, Subject, merge, combineLatest } from 'rxjs';
 import { map, filter, mergeMap, startWith } from 'rxjs/operators';
 import { MergeMapOperator } from 'rxjs/internal/operators/mergeMap';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-band-list',
   templateUrl: './band-list.component.html',
   styleUrls: ['./band-list.component.sass']
 })
-export class BandListComponent implements OnInit {
+export class BandListComponent {
   bandList$: Observable<{band: Band[], isTriggered: boolean}>; 
   refreshDataClickSubject = new Subject(); 
+  // test: Observable<{}> = 
 
-  constructor(private bandDataService: BandDataService) { 
+  constructor(private bandDataService: BandDataService, private activedRoute: ActivatedRoute) { 
   // this.bandList$ = this.bandDataService.getBands();  
-  }
-
-  ngOnInit() {
-    const refreshedData = this.refreshDataClickSubject.asObservable();   
+   
+ 
+    const refreshedData = this.refreshDataClickSubject.asObservable();
+    // Gets ovbservable of event emitted by button click   
     const refreshTrigger$ = refreshedData.pipe(
       startWith({})
-    )
+      //combineLatest(activedRoute.queryParams),
+      // map(([_, params])=>{
+      //   if (params.active === undefined) return undefined; 
+      //   return params.active ==="true"; 
+      // })
+  
+
+    );
    
     const bandList = refreshTrigger$.pipe( 
       mergeMap(() => this.bandDataService.getBands())
     )
 
     // Using Merge to merge observables 
-    
    this.bandList$ = merge (
     refreshTrigger$.pipe(map(() => ({band: [], isTriggered: true}))),
     bandList.pipe(map(value => ({band: value, isTriggered: false}))),
